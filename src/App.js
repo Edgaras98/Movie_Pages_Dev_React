67552c823f6f086ea7db872b64d9a65ca1914393
Edgaras98 <edgarasstepanovas14@gmail.com>
2021-10-27 18:13:52 +0300

    comit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-function App() {
+import React, { useEffect, useState } from "react";
+import SignUp from "./components/SignUp";
+import Movies from "./components/Movies";
+import LogIn from "./components/LogIn";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "./Context/AuthContext";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import ForgotPassword from "./components/ForgotPassword";
+
+export default function App() {
   return (
-    <div className="App">
-      <h2>Hello world{2 + 2}</h2>
-    </div>
+    <>
+      <div>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path="/" component={Movies} />
+              <Route path="/signup" component={SignUp} />
+              <Route path="/login" component={LogIn} />
+              <Route path="/forgot-password" component={ForgotPassword} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </>
   );
 }
-
-export default App;
