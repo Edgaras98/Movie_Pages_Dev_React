294ab8ba5b4822e42d146b64985ab577f4d342aa
Edgaras98 <edgarasstepanovas14@gmail.com>
2021-10-28 16:43:10 +0300

    init

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { HashRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
